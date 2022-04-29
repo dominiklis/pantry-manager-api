b3c130ee8c73f6432d047f4bed1b4518ed06ba85
dominiklis <lis.dominik11@gmail.com>
2022-04-29 18:29:50 +0200

    camelize nested data

--- db/index.js
+++ db/index.js
@@ -11,15 +11,30 @@ const {
   ShoppingListsItems,
 } = require("./repos");
 
+const replacePropertiesWithCamelized = (camelCaseName, originalName, data) => {
+  for (let i = 0; i < data.length; i++) {
+    const d = data[i];
+    d[camelCaseName] = d[originalName];
+    delete d[originalName];
+  }
+
+  return data;
+};
+
 const camelizeColumns = (data) => {
-  const tmp = data[0];
-  for (const prop in tmp) {
-    const camel = pgp.utils.camelize(prop);
-    if (!(camel in tmp)) {
-      for (let i = 0; i < data.length; i++) {
-        const d = data[i];
-        d[camel] = d[prop];
-        delete d[prop];
+  const tmp = data?.[0];
+
+  if (tmp && typeof tmp === "object") {
+    for (const prop in tmp) {
+      if (Array.isArray(tmp[prop])) {
+        for (let i = 0; i < data.length; i++) {
+          camelizeColumns(data[i][prop]);
+        }
+      }
+
+      const camel = pgPromise.utils.camelize(prop);
+      if (!(camel in tmp)) {
+        replacePropertiesWithCamelized(camel, prop, data);
       }
     }
   }
