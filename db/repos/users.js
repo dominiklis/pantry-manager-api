dc508441cb3bdc7a0ff0ea7ed4e68bb486e1b948
dominiklis <lis.dominik11@gmail.com>
2022-12-11 18:30:24 +0100

    change normal strings to template strings in users repo

--- db/repos/users.js
+++ db/repos/users.js
@@ -6,25 +6,27 @@ class UsersRepository {
 
   async create(userName, email, password) {
     return this.db.oneOrNone(
-      "INSERT INTO users(user_name, email, password) VALUES($1, $2, $3) RETURNING user_id, user_name, email",
+      `INSERT INTO users(user_name, email, password) VALUES($1, $2, $3) 
+        RETURNING user_id, user_name, email`,
       [userName, email, password]
     );
   }
 
   async findByUserNameOrEmail(userName, email) {
     return this.db.oneOrNone(
-      "SELECT * FROM users WHERE user_name=$1 OR email=$2;",
+      `SELECT * FROM users WHERE user_name=$1 OR email=$2;`,
       [userName, email]
     );
   }
 
   async findById(userId) {
-    return this.db.oneOrNone("SELECT * FROM users WHERE user_id=$1;", [userId]);
+    return this.db.oneOrNone(`SELECT * FROM users WHERE user_id=$1;`, [userId]);
   }
 
   async edit(userId, userName, email, password) {
     return this.db.oneOrNone(
-      `UPDATE users SET user_name=$2, email=$3, password=$4 WHERE user_id=$1 RETURNING *`,
+      `UPDATE users SET user_name=$2, email=$3, password=$4 
+        WHERE user_id=$1 RETURNING *`,
       [userId, userName, email, password]
     );
   }
